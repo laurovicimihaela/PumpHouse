51c674150fc92056324c9555156f1357f207a285
sebastianiscruleasa <sebi.iscruleasa@yahoo.ro>
2022-05-19 17:08:29 +0300

    main page mobile responsive added

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,17 +1,19 @@
 import Layout from "./components/Layout/Layout";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Classes from "./pages/Classes";
+import MainPage from "./pages/MainPage";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Layout>
-        <Routes>
-          <Route path="/classes" element={<Classes />} />
-        </Routes>
-      </Layout>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Layout>
+                <Routes>
+                    <Route path="/" element={<MainPage />} />
+                    <Route path="/classes" element={<Classes />} />
+                </Routes>
+            </Layout>
+        </BrowserRouter>
+    );
 }
 
 export default App;
