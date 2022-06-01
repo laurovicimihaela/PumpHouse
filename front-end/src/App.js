0b6a16438995a9dc6985255a411f30eda990ddf7
Mihai405 <mihaijurj2000@yahoo.com>
2022-06-01 18:58:26 +0300

    Modify Navbar depending on role

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -30,6 +30,7 @@ function App() {
           <Route path="/mygyms" element={<MyGyms />} />
           <Route path="/addgym" element={<AddGym />} />
           <Route path="/addclass" element={<AddClass />} />
+          <Route path="*" element={<MainPage />} />
         </Routes>
       </Layout>
     </BrowserRouter>
